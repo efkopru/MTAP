b3bb90f1e013d99eea91ff03b3fd2b14d6992f12
Esad Kopru <efkopru@yahoo.com>
2021-02-08 12:37:12 -0600

    Changes on plotting raster surface.

--- R/MTAPMain.R
+++ R/MTAPMain.R
@@ -1,30 +1,17 @@
 #
 # Author: Esad Kopru
 #
-# Explanation of function:
-# params :
-# outs:
-#
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
 #
 
-library(knitr)
-library(dplyr)
 library(ggplot2)
 library(ompr)
 library(sf)
 library(sp)
 library(spdep)
-library(magrittr)
+library(dplyr)
 library(ROI.plugin.glpk)
 library(ROI.plugin.symphony)
 library(ompr.roi)
-library(tidyverse)
 
 set.seed(12345)
 # Plot integer axis.
@@ -489,11 +476,13 @@ solveMTAPInstance <- function(n, neigType, upperLimit){
 #'
 #' @return none
 #' @export none
+#'
 plotMTAPResults<- function(MTAPResult, neigType){
   solution <- get_solution(MTAPResult, d[i]) %>%
     filter(value > 0)
 
   d <- data.frame(impedances[solution$i,])
+  d2 <- data.frame(index=d$index, x=d$x-0.5, y= d$y-0.5, nX=d$nX-0.5, nY=d$nY-0.5, impValues = d$impValues)
 
   ##################
   # Plot the solution
@@ -506,17 +495,315 @@ plotMTAPResults<- function(MTAPResult, neigType){
   }
 
 
+  allP2 <- data.frame(idx=allPoints$idx, x=allPoints$x-0.5, y=allPoints$y-0.5, grp=allPoints$grp)
+
   ggplot() +
     # if in aes(), size goes in the legend.
-    geom_point(data=allPoints, aes(x, y, color=grp),size = 4) +
+    geom_point(data=allP2, aes(x, y, color=grp),size = 3) +
     # use minor_breaks for background grid lines.
     scale_y_continuous(minor_breaks = integer_breaks) +
     scale_x_continuous(minor_breaks = integer_breaks) +
     scale_color_manual(values=c("green", "red", "gray"), breaks = c("Start", "Target", "Intermediate")) +
-    geom_segment(data=d, mapping = aes(x=x, y=y, xend=nX, yend=nY), arrow=arrow(length=unit(0.21,"cm"), ends="last", type = "closed"), col='blue', size=0.1)+
-    geom_text(data=d, mapping = aes(x=nX+xOffset, y=nY+yOffset,label=impValues) )+
+    geom_segment(data=d2, mapping = aes(x=x, y=y, xend=nX, yend=nY), arrow=arrow(length=unit(0.21,"cm"), ends="last", type = "closed"), col='blue', size=0.1)+
+    # geom_text(data=d2, mapping = aes(x=nX+xOffset, y=nY+yOffset,label=impValues) )+
     labs( title = paste0("Optimal Cost : ", MTAPResult$objective_value), subtitle = paste0("Computation Time : ",compTime, " secs"))+
     # To remove background grid lines
-    theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank())
-
+    theme(  panel.background = element_rect(fill = "lightgray",
+                                            colour = "lightgray",
+                                            size = 0.5, linetype = "solid"),
+            panel.grid.major = element_line(size = 0.5, linetype = 'solid',
+                                            colour = "white"),
+            panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
+                                            colour = "white"))
 }
+
+
+#
+#' Reads a shapefile and creates a topology based on its dataframe .
+#'
+#'
+#' @param MTAPResult result of MTAP
+#' @param neigType queen-rook
+#'
+#' @return none
+#' @export none
+
+MTAPonShapefile <- function(shpFilePath, neigType, howManyTargetPts, upperLimit){
+  ##
+  ## Read Shapefiles
+  ##
+
+  inShp <- rgdal::readOGR(dsn=shpFilePath, integer64="warn.loss")
+  inShp.centroid <- coordinates(inShp)        # Get province centroids
+
+  # Add lat, long to input shapefile.
+  inShp$x <- inShp.centroid[,1]
+  inShp$y <- inShp.centroid[,2]
+
+
+  createNeighbors <- function(inShp, neigType, upperLimit){
+    proj4string(inShp)                          # map projection
+    inShp.bbox <- bbox(inShp)           # province bounding box for map region
+
+    isQueen = F
+    if(tolower(neigType)=='queen'){
+      isQueen = T
+    }
+
+    inShp.link <- poly2nb(inShp, queen=isQueen)
+    # Create neighborhood lists
+    neighDF <- data.frame()
+    for(i in 1:length(inShp.link)){
+      a <- inShp.link[[i]]
+      if(a != 0){
+        for(j in 1:length(a)){
+          neighDF <- rbind(neighDF, list(i, a[j], inShp@data$x[i] ,inShp@data$y[i]
+                                         ,inShp@data$x[a[j]] ,inShp@data$y[a[j]]))
+        }
+      }
+
+    }
+    names(neighDF)[1] <- "F"
+    names(neighDF)[2] <- "T"
+    names(neighDF)[3] <- "x"
+    names(neighDF)[4] <- "y"
+    names(neighDF)[5] <- "nX"
+    names(neighDF)[6] <- "nY"
+
+    return(neighDF)
+  }
+
+
+  # howManyTargetPts <- 4
+  # Record all points
+  sizeOfinput <- nrow(inShp@data)
+  allPoints <- data.frame(idx = 1:sizeOfinput, x = inShp@data$x, y = inShp@data$y, grp="Intermediate")
+
+  # decide on start and target points
+  # Set Start and Target points
+  # If Texas
+  if(length(inShp@polygons) == 254){
+    # 226 - houston
+    # 216 - austin
+    # 130 - san saba
+    # 155 - wichita
+    # 239 - dallas
+
+    # Record all points
+    sizeOfinput <- nrow(inShp@data)
+    allPoints <- data.frame(idx = 1:sizeOfinput, x = inShp@data$x, y = inShp@data$y, grp="Intermediate")
+
+    startPoint <- data.frame()
+    startPoint <- data.frame(x = allPoints[226,2], y = allPoints[226,3])
+    targetPoints <- data.frame()
+    targetPoints <- data.frame(x = allPoints[216,2], y = allPoints[216,3])
+    targetPoints <- rbind(targetPoints, c(x = allPoints[130,2], y = allPoints[130,3]))
+    targetPoints <- rbind(targetPoints, c(x = allPoints[155,2], y = allPoints[155,3]))
+    targetPoints <- rbind(targetPoints, c(x = allPoints[239,2], y = allPoints[239,3]))
+
+  }else {
+    tNum <- 32
+    targetPoints <- data.frame()
+    startPoint <- data.frame(x = allPoints[5,2], y = allPoints[5,3])
+    for(i in 1:howManyTargetPts){
+      tps <- floor(sizeOfinput/tNum)
+      if(tps == 5){
+        tps <- tps + 3
+      }
+      targetPoints <- rbind(targetPoints, c(x = allPoints[tps,2], y = allPoints[tps,3]))
+      tNum <- tNum/2
+    }
+    names(targetPoints)[1] <- "x"
+    names(targetPoints)[2] <- "y"
+  }
+
+
+  # Number of Start and Target points
+  numOfTargetPts <- nrow(targetPoints)
+  numOfStartPts <- nrow(startPoint)
+
+
+  # Group Start, Target and Int nodes for plotting
+  for(i in 1:numOfStartPts){
+    allPoints[which(allPoints$x == startPoint$x[i] & allPoints$y == startPoint$y[i]), "grp"] <- "Start"
+  }
+
+  for(i in 1:numOfTargetPts){
+    allPoints[which(allPoints$x == targetPoints$x[i] & allPoints$y == targetPoints$y[i]), "grp"] <- "Target"
+
+  }
+
+  shapeDF <- createNeighbors(inShp, neigType, upperLimit)
+  nrow(shapeDF)
+  impedances <- shapeDF
+
+  # Total number of variables
+  totNV <- nrow(impedances)
+  index <- 1:totNV
+
+  # Create random impedance between 1-20
+  impValues <- sample(1:upperLimit, totNV, replace = T)
+  impedances <- cbind(index, impedances, impValues, impValues)
+
+  flowVars <- cbind(index,"flow", "f", neigType, shapeDF)
+
+  decisionVars <- cbind(index,"decision", "d", neigType, shapeDF)
+
+  dummyVars <- cbind(index, "dummy", "u", neigType, shapeDF)
+
+
+  ########################### CALCULATE MTAP ###########################
+
+  # Create a new MIPModel()
+  MTAPModel <- MIPModel()
+
+  # Flow variables
+  MTAPModel <- add_variable(MTAPModel, f[i], i = 1:totNV, type = "integer", lb=0)
+
+  # Decision variables
+  MTAPModel <- add_variable(MTAPModel, d[i], i = 1:totNV, type = "binary")
+
+  # Dummy variables
+  MTAPModel <- add_variable(MTAPModel, u[i], i = 1:totNV, type = "binary")
+
+
+  # minimize travel distance
+  MTAPModel <- set_objective(MTAPModel, sum_expr(d[i] * impedances[i, 8]  , i = 1:totNV), "min")
+
+  ####################################################
+  # Subject TO:
+  ####################################################
+
+  # Start point constraints:  all Flow out = # of target points
+  for (i in 1:numOfStartPts) {
+    # FROM and TO Start point Indexes
+    FROMstartPoint <- flowVars[which(flowVars$x == startPoint$x[i] & flowVars$y == startPoint$y[i]), "index" ]
+    # TOstartPoint <- flowVars[which(flowVars$nX == startPoint$x[i] & flowVars$nY == startPoint$y[i]), "index" ]
+    MTAPModel <- add_constraint(MTAPModel, sum_expr(f[i], i = FROMstartPoint) == numOfTargetPts)
+
+  }
+
+  ####################################################
+  # Target point constraints: all Flow in = 1
+  ####################################################
+
+  for(i in 1:numOfTargetPts){
+    # To Target Point Indexes
+    ToTargetPoint <- flowVars[which(flowVars$nX == targetPoints$x[i] & flowVars$nY == targetPoints$y[i]), "index" ]
+    MTAPModel <- add_constraint(MTAPModel, sum_expr(f[i], i = ToTargetPoint ) == 1)
+
+  }
+
+  ####################################################
+  ################## Intermediate Nodes ##############
+  ####################################################
+  for(i in 1:numOfStartPts){
+    intNodes <- flowVars
+    # Remove To Start Point
+    intNodes <- intNodes[-which(flowVars$nX==startPoint$x[i] & flowVars$nY==startPoint$y[i]),]
+    nrow(intNodes)
+  }
+
+
+  for(i in 1:numOfTargetPts){
+    # Remove From Target Points
+    intNodes <- intNodes[-which(intNodes$x == targetPoints$x[i] & intNodes$y == targetPoints$y[i]),]
+    nrow(intNodes)
+  }
+
+
+  # Intermediate nodes constraints (excluding start and target points), all Flow in == all Flow out
+  IntNodesCondition <- ""
+  for(s in 1:numOfStartPts){
+    IntNodesCondition <- paste(IntNodesCondition, "(r != startPoint$x[",s,"] | c != startPoint$y[",s,"]) &")
+  }
+  for(t in 1:numOfTargetPts){
+    if( t == numOfTargetPts){
+      IntNodesCondition <- paste(IntNodesCondition, "(r != targetPoints$x[",t,"] | c != targetPoints$y[",t,"])")
+    }
+    else{
+      IntNodesCondition <- paste(IntNodesCondition, "(r != targetPoints$x[",t,"] | c != targetPoints$y[",t,"]) &")
+    }
+  }
+
+  for(row in 1:nrow(intNodes)){
+    r <- intNodes$x[row]
+    c <- intNodes$y[row]
+    if(eval(parse(text=IntNodesCondition))){
+      fin <- c(intNodes[(which(intNodes$x==r & intNodes$y == c)), "index"])
+      fout <- c(intNodes[(which(intNodes$nX==r & intNodes$nY == c)), "index"])
+      MTAPModel <- add_constraint(MTAPModel, sum_expr(f[a], a = fin ) == sum_expr(f[b], b = fout ))
+    }
+  }
+
+  ####################################################
+  # Dummy constraints.
+  ####################################################
+  MTAPModel <- add_constraint(MTAPModel, f[k] == d[k] + u[k], k = 1:totNV)
+  MTAPModel <- add_constraint(MTAPModel, d[k] >= u[k], k = 1:totNV)
+
+
+  # Solve an instance and calculate solution time
+  start_time <- Sys.time()
+
+  # Solution with GLPK solver
+  result <- solve_model(MTAPModel, with_ROI(solver = "glpk"))
+  end_time <- Sys.time()
+  compTime <- round(difftime(end_time, start_time, units="secs"), 4)
+
+
+
+  solution <- get_solution(result, d[i]) %>%
+    filter(value > 0)
+
+  d <- data.frame(impedances[solution$i,])
+
+  d$grp <- allPoints[d$F,]$grp
+
+  d
+
+  ##################
+  # Plot the solution
+  if(tolower(neigType)=='hexagon'){
+    xOffset <- 0
+    yOffset <- 0.07
+  }else{
+    xOffset <- 0.1
+    yOffset <- 0.1
+  }
+
+
+  # Alternative plotting with arrows - does not look as attractive
+  plot(inShp,col="palegreen3" ,border=grey(0.9), axes=T ) # Second plot areas
+  arrows(x0 = d$x, x1 = d$nX, y0 = d$y, y1 = d$nY, length = 0.3, angle = 30 , col="blue")
+
+
+  # # Alternative plotting, centroids
+  # mapAlternative <- ggplot() +
+  #   # if in aes(), size goes in the legend.
+  #   geom_point(data=allPoints, aes(x, y, color=grp),size = 2) +
+  #   # use minor_breaks for background grid lines.
+  #   scale_y_continuous(minor_breaks = integer_breaks) +
+  #   scale_x_continuous(minor_breaks = integer_breaks) +
+  #   scale_color_manual(values=c("green", "red", "gray"), breaks = c("Start", "Target", "Intermediate")) +
+  #   geom_segment(data=d, mapping = aes(x=x, y=y, xend=nX, yend=nY), arrow=arrow(length=unit(0.21,"cm"), ends="last", type = "closed"), col='blue', size=0.1)+
+  #   geom_text(data=d, mapping = aes(x=nX+xOffset, y=nY+yOffset,label=impValues) )+
+  #   labs( title = paste0("Optimal Cost : ", sum(d$impValues)), subtitle = paste0("Computation Time : ",compTime, " secs"))+
+  #   # To remove background grid lines
+  #   # theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+  #   theme_classic()
+  #
+  # mapAlternative
+
+  # Plot the results
+  map <- ggplot() +
+    geom_polygon(data = inShp, aes(x = long, y = lat, group = group), colour = "gray", fill = NA) +
+    geom_point(data=targetPoints, aes(x, y), color="red",size = 4) +
+    geom_point(data=startPoint, aes(x, y), color="green",size = 4) +
+    labs( title = paste0("Optimal Cost : ", sum(d$impValues)), subtitle = paste0("Computation Time : ",compTime, " secs"))+
+    geom_segment(data=d, mapping = aes(x=x, y=y, xend=nX, yend=nY), arrow=arrow(length=unit(0.21,"cm"), ends="last", type = "closed"), col="blue", size=0.1) +
+    theme(panel.background = element_blank(), panel.border = element_rect(colour = "black", fill=NA, size=1))
+
+  map
+}
+
